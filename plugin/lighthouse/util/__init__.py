3170f88296308194837e01fa24092b639b259a45
gaasedelen <markus.gaasedelen@gmail.com>
2017-06-14 21:52:57 -0700

    created the composition cache

--- plugin/lighthouse/util/__init__.py
+++ plugin/lighthouse/util/__init__.py
@@ -4,6 +4,7 @@ import cProfile
 import idaapi
 
 from ida import *
+from .misc import CompositionCache
 from log import lmsg, logging_started, start_logging
 from qtshim import using_pyqt5, QtCore, QtGui, QtWidgets
 
