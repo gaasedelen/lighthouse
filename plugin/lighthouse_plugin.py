4eb0b445eda3353597160476574908d4c7660d3c
gaasedelen <markus.gaasedelen@gmail.com>
2017-12-07 05:27:15 -0500

    version bump to v0.7.0

--- plugin/lighthouse_plugin.py
+++ plugin/lighthouse_plugin.py
@@ -20,7 +20,7 @@ if not logging_started():
 # IDA Plugin
 #------------------------------------------------------------------------------
 
-PLUGIN_VERSION = "0.6.1"
+PLUGIN_VERSION = "0.7.0"
 AUTHORS        = "Markus Gaasedelen"
 DATE           = "2017"
 
