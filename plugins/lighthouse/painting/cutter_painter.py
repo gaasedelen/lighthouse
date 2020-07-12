7733cbd002fda7d2acbb2e30f9403e0f9e60ccff
E <edelpeon@gmail.com>
2020-07-11 21:26:02 -0300

    Fixed highlighting

--- plugins/lighthouse/painting/cutter_painter.py
+++ plugins/lighthouse/painting/cutter_painter.py
@@ -41,26 +41,45 @@ class CutterPainter(DatabasePainter):
     def _clear_instructions(self, instructions):
         self._action_complete.set()
 
+    def _partial_paint(self, addresses, color):
+        highlighter = disassembler[self.lctx]._core.getBIHighlighter()
+        for address in addresses:
+            logger.debug('Partially painting {}'.format(address))
+            highlighter.clear(address)
+
+
     def _paint_nodes(self, nodes_addresses):
-        #b, g, r = to_rgb(self.palette.coverage_paint)
         (r, g, b, _) = self.palette.coverage_paint.getRgb()
         color = QtGui.QColor(r, g, b)
+        color_partial = QtGui.QColor(r, 0, 0)
 
         for node_address in nodes_addresses:
-            #node_metadata = self.director.metadata.nodes.get(node_address, None)
-            logger.debug('Logging CutterBindings {} {} {} {}'
-                   .format(disassembler,
-                           disassembler[self.lctx],
-                           disassembler[self.lctx]._core,
-                           disassembler[self.lctx]._core.getBBHighlighter()))
-            disassembler[self.lctx]._core.getBBHighlighter().highlight(self.director.coverage.nodes.get(node_address, None), color)
-            self._painted_nodes.add(node_address)
+            node_metadata = self.director.metadata.nodes.get(node_address, None)
+            node_coverage = self.director.coverage.nodes.get(node_address, None)
+
+
+            # Database unsync. Abort
+            if not (node_coverage and node_metadata):
+                logger.warning('Unsynced database. Aborting')
+                self._msg_queue.put(self.MSG_ABORT)
+                node_addresses = node_addresses[:node_addresses.index(node_address)]
+                break
+
+            # Node completely executed
+            if node_coverage.instructions_executed == node_metadata.instruction_count:
+                logger.debug('Painting node {}'.format(node_address))
+                disassembler[self.lctx]._core.getBBHighlighter().highlight(node_address, color)
+                self._painted_nodes.add(node_address)
+
+            # Partially executed nodes
+            else:
+                self._partial_paint(node_coverage.executed_instructions.keys(), color)
+
         self._action_complete.set()
 
     def _clear_nodes(self, addresses):
         for address in addresses:
             disassembler[self.lctx]._core.getBBHighlighter().clear(address)
-            #disassembler.unhighlight(address)
             self._painted_nodes.discard(address)
         self._action_complete.set()
 
