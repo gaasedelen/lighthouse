374ca999f81f92bbc85706498c59d3e86a0cb0a5
gaasedelen <markus.gaasedelen@gmail.com>
2021-08-03 12:31:10 -0400

    maybe fixes qt conflict issues? (issue #100)

--- plugins/lighthouse/util/qt/shim.py
+++ plugins/lighthouse/util/qt/shim.py
@@ -16,19 +16,40 @@ QT_AVAILABLE = False
 #
 #    this file was critical for retaining compatibility with Qt4 frameworks
 #    used by IDA 6.8/6.95, but it less important now. support for Qt 4 and
-#    older versions of IDA will be deprecated in Lighthouse v0.9.0
+#    older versions of IDA (< 7.0) were deprecated in Lighthouse v0.9.0
 #
 
 USING_PYQT5 = False
 USING_PYSIDE2 = False
 USING_PYSIDE6 = False
 
+#
+#    TODO/QT: This file is getting pretty gross. this whole shim system
+#    should probably get refactored as I really don't want disassembler
+#    specific dependencies in here...
+#
+
+try:
+    import ida_idaapi
+    USING_IDA = True
+except ImportError:
+    USING_IDA = False
+
+try:
+    import binaryninjaui
+    USING_NEW_BINJA = "qt_major_version" in binaryninjaui.__dict__ and binaryninjaui.qt_major_version == 6
+    USING_OLD_BINJA = not(USING_NEW_BINJA)
+except ImportError:
+    USING_NEW_BINJA = False
+    USING_OLD_BINJA = False
+
 #------------------------------------------------------------------------------
 # PyQt5 Compatibility
 #------------------------------------------------------------------------------
 
-# attempt to load PyQt5
-if QT_AVAILABLE == False:
+# attempt to load PyQt5 (IDA 7.0+)
+if USING_IDA:
+
     try:
         import PyQt5.QtGui as QtGui
         import PyQt5.QtCore as QtCore
@@ -46,8 +67,9 @@ if QT_AVAILABLE == False:
 # PySide2 Compatibility
 #------------------------------------------------------------------------------
 
-# if PyQt5 did not import, try to load PySide
-if QT_AVAILABLE == False:
+# if PyQt5 did not import, try to load PySide2 (Old Binary Ninja / Cutter)
+if not QT_AVAILABLE and USING_OLD_BINJA:
+
     try:
         import PySide2.QtGui as QtGui
         import PySide2.QtCore as QtCore
@@ -69,8 +91,9 @@ if QT_AVAILABLE == False:
 # PySide6 Compatibility
 #------------------------------------------------------------------------------
 
-# If all else fails, try to load PySide6
-if QT_AVAILABLE == False:
+# If all else fails, try to load PySide6 (New Binary Ninja)
+if not QT_AVAILABLE and USING_NEW_BINJA:
+
     try:
         import PySide6.QtGui as QtGui
         import PySide6.QtCore as QtCore
